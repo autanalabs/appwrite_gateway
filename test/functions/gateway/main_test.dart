78f3a49c7593249024368e0812c8c296c9232dd7
gb <gb@autanalabs.com>
2024-02-22 15:50:26 -0300

    folder refactor

--- test/functions/gateway/main_test.dart
+++ test/functions/gateway/main_test.dart
@@ -3,7 +3,7 @@ import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import '../../../functions/gateway/main.dart' as sut;
+import '../../../lib/main.dart' as sut;
 
 @GenerateNiceMocks([MockSpec<http.Client>()])
 import 'main_test.mocks.dart';
