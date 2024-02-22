6e8e93bb043603c91e29fe202eb612a9b6a72955
gb <gb@autanalabs.com>
2024-02-22 17:55:34 -0300

    added appwrite lib

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:dart_appwrite/dart_appwrite.dart' as appwrite;
 
 const access_key = "ca39f98b01df960c01efdf1520609537";
 
@@ -10,8 +12,15 @@ http.Client get httpClient => _httpClient ?? http.Client();
 
 set httpClient(http.Client client) => _httpClient = client;
 
+late final appwrite.Client client;
+late final appwrite.Database database;
 
 Future<dynamic> main(final context) async {
+
+  context.log("DEBUG: req.body = ${context.req.body}");
+
+  final String apiKey = context.req.body;
+  await init(apiKey);
   if (context.req.path == '/eur') {
     final amountInEuros = double.parse(context.req.query['amount']);
     final response = await httpClient.get(Uri.parse('http://api.exchangerate.host/live?source=EUR&currencies=USD&access_key=$access_key'));
@@ -32,4 +41,13 @@ Future<dynamic> main(final context) async {
           'ok': false, 
           'message': 'Invalid currency ${context.req.path}.'
         }, 400);
+}
+
+Future init(String apiKey) async {
+  client = appwrite.Client()
+        .setEndpoint('https://cloud.appwrite.io/v1')
+        .setProject(Platform.environment['APPWRITE_PROJECT_ID'])
+        .setKey(apiKey);
+
+  database = appwrite.Database(client);
 }
