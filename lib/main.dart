beeacf978eac6b50c9971a728d5c5a1c564ad473
gb <gb@autanalabs.com>
2024-02-23 16:48:37 -0300

    release 0.8

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ set httpClient(http.Client client) => _httpClient = client;
 
 Future<dynamic> main(final context) async {
 
-  context.log("version: 0.7");
+  context.log("version: 0.8");
   // context.log('headers: ${context.req.headers}');
   // context.log('auth header: ${context.req.headers['authorization']}');
 
